539d3531221d958c32a94bbb45f0db62156c30d8
maycon silva <maycon8609@gmail.com>
2024-02-07 21:25:42 -0300

    feat: create certification student repository

--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,16 +1,20 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
+  @Autowired
+  private CertificationStudentRepository certificationStudentRepository;
+
   public boolean execute(VerifyHasCertificationDTO dto) {
-    if (
-      dto.getEmail().equals("johndoe@email.com") &&
-      dto.getTechnology().equals("java")
-    ) {
+    var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+    
+    if (!result.isEmpty()) {
       return true;
     }
 
